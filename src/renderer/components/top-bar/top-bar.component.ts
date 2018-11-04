7cf836d272ed781f52c1d0c2d923ae778646cd67
Giorgio Garasto <giorgio@garasto.it>
2018-11-04 20:49:01 +0100

    feature: Top bar layout improvements

--- src/renderer/components/top-bar/top-bar.component.ts
+++ src/renderer/components/top-bar/top-bar.component.ts
@@ -1,8 +1,7 @@
-import { navigate } from '@actions/app';
 import sharedStyles from '@components/shared.styles';
 import { html, LitElement, property, PropertyValues } from '@polymer/lit-element';
 import { RootState, store } from '@store';
-import { connect, installRouter, updateMetadata } from 'pwa-helpers';
+import { connect } from 'pwa-helpers';
 import styles from './top-bar.styles';
 
 class TopBar extends connect(store)(LitElement) {
@@ -22,27 +21,16 @@ class TopBar extends connect(store)(LitElement) {
       ${styles}
       <img src="https://upload.wikimedia.org/wikipedia/commons/c/cb/Google_Assistant_logo.svg">
       <div class="heading">
-        google_logo Assistant Desktop
-        <span>Unofficial</span>
+        google_logo
+        <span class="ad">Assistant Desktop</span>
+        <span class="unofficial">Unofficial</span>
       </div>
       <div class="spacer"></div>
       <div class="material-icons-extended">more_vert</div>
     `;
   }
 
-  protected firstUpdated() {
-    installRouter((location) => store.dispatch(navigate(decodeURIComponent(location.pathname))));
-  }
-
-  protected updated(changedProps: PropertyValues) {
-    if (changedProps.has('_page')) {
-      const pageTitle = this.appTitle + ' - ' + this._page;
-      updateMetadata({
-        description: pageTitle,
-        title: pageTitle,
-      });
-    }
-  }
+  protected updated(changedProps: PropertyValues) {}
 }
 
 window.customElements.define('gad-top-bar', TopBar);
