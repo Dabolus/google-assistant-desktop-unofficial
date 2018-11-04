4041b3694b10a64d1fd3a0591fb191aa1883ce3f
Giorgio Garasto <giorgio@garasto.it>
2018-11-04 21:04:33 +0100

    feature: Add support for static assets

--- webpack/renderer.base.config.ts
+++ webpack/renderer.base.config.ts
@@ -1,6 +1,6 @@
 /* tslint:disable:object-literal-sort-keys */
 /// <reference types="../typings" />
-// import CopyPlugin from 'copy-webpack-plugin';
+import CopyPlugin from 'copy-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { resolve } from 'path';
 import ScriptExtHtmlPlugin from 'script-ext-html-webpack-plugin';
@@ -46,13 +46,13 @@ const config: Configuration = smartMerge(baseConfig, {
     ],
   },
   plugins: [
-    /* new CopyPlugin([
+    new CopyPlugin([
       // Assets
       {
-        from: resolve(__dirname, '../src/assets'),
-        to: '.',
+        from: resolve(__dirname, '../src/renderer/assets'),
+        to: 'assets',
       },
-    ]), */
+    ]),
     new MiniCssExtractPlugin({
       filename: 'styles/[name].css',
       chunkFilename: 'styles/[id].css',
