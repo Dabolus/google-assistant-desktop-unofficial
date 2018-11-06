61c52d8e388daf4420fe38a62350e2acc6d9fef5
Giorgio Garasto <giorgio@garasto.it>
2018-11-06 23:05:54 +0100

    chore: Remove empty code blocks

--- src/renderer/components/chat/chat.component.ts
+++ src/renderer/components/chat/chat.component.ts
@@ -5,16 +5,12 @@ import { connect } from 'pwa-helpers';
 import styles from './chat.styles';
 
 export class Chat extends connect(store)(LitElement) {
-  public stateChanged(state: RootState) {}
-
   protected render() {
     return html`
       ${sharedStyles}
       ${styles}
     `;
   }
-
-  protected updated(changedProps: PropertyValues) {}
 }
 
 window.customElements.define('gad-chat', Chat);
