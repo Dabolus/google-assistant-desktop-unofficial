edd08f390ffaa49b017fbe1449306e5c00c8e9a4
Giorgio Garasto <giorgio@garasto.it>
2019-03-26 22:14:46 +0100

    feature: Make top bar more googlish

--- src/renderer/components/top-bar/top-bar.template.ts
+++ src/renderer/components/top-bar/top-bar.template.ts
@@ -3,12 +3,8 @@ import { TopBar } from './top-bar.component';
 
 export default function template(this: TopBar) {
   return html`
-    <img src="assets/google-assistant-logo.svg">
-    <div class="heading">
-      google_logo
-      <span class="ad">Assistant Desktop</span>
-      <span class="unofficial">Unofficial</span>
-    </div>
+    <img src="assets/google-logo.svg">
+    <div class="heading">Assistant</div>
     <div class="spacer"></div>
     <div class="material-icons-extended" @click="${this._menuButtonClicked}">more_vert</div>
     <div class="menu" role="menu" ?hidden="${!this._menuOpened}">The menu!</div>
