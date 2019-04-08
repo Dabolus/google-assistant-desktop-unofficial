31cf0bee98ef8240a2a4fb540af8be70f930b9fe
Giorgio Garasto <giorgio@garasto.it>
2019-04-08 20:34:30 +0200

    feat: Setup authenticate saga

--- src/renderer/store/auth/auth.sagas.ts
+++ src/renderer/store/auth/auth.sagas.ts
@@ -1,1 +1,22 @@
-export const authSagas = [];
+import { container } from '@helpers/di.helper';
+import { Auth, AuthService } from '@services/auth.service';
+import { call, put, takeLatest } from 'redux-saga/effects';
+import { AuthActionAuthenticateRequested, AuthActionType, resolveAuthentication } from './auth.actions';
+
+const authService: Auth = container.get(AuthService);
+
+function* handleUserAuthentication({
+  payload: { clientId, clientSecret },
+}: AuthActionAuthenticateRequested) {
+  const req = yield call(authService.authenticate, clientId, clientSecret);
+  console.log('req', req);
+  yield put(resolveAuthentication());
+}
+
+function* authenticate() {
+  yield takeLatest(AuthActionType.AUTHENTICATE_REQUESTED, handleUserAuthentication);
+}
+
+export const authSagas = [
+  authenticate,
+];
