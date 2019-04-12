cb8dbb5d4a2b9c945973f06c3d6beec53e44789f
Giorgio Garasto <giorgio@garasto.it>
2019-04-12 21:30:25 +0200

    feat: Add update locale actions

--- src/renderer/store/app/app.actions.ts
+++ src/renderer/store/app/app.actions.ts
@@ -1,9 +1,15 @@
+import { LocaleData } from '@locales/model';
 import { FluxStandardAction } from '../store.model';
+import { Locale } from './app.model';
 
 // Action types
 export enum AppActionType {
   UPDATE_PAGE = 'UPDATE_PAGE',
   UPDATE_MENU_STATE = 'UPDATE_MENU_STATE',
+
+  UPDATE_LOCALE_REQUESTED = 'UPDATE_LOCALE_REQUESTED',
+  UPDATE_LOCALE_RESOLVED = 'UPDATE_LOCALE_RESOLVED',
+  UPDATE_LOCALE_REJECTED = 'UPDATE_LOCALE_REJECTED',
 }
 
 // Action interfaces
@@ -21,7 +27,33 @@ export interface AppActionUpdateMenuState extends
   };
 }
 
-export type AppAction = AppActionUpdatePage | AppActionUpdateMenuState;
+export interface AppActionUpdateLocaleRequested extends
+  FluxStandardAction<AppActionType.UPDATE_LOCALE_REQUESTED> {
+  payload: {
+    locale: Locale;
+  };
+}
+
+export interface AppActionUpdateLocaleResolved extends
+  FluxStandardAction<AppActionType.UPDATE_LOCALE_RESOLVED> {
+  payload: {
+    localeData: LocaleData;
+  };
+}
+
+export interface AppActionUpdateLocaleRejected extends
+  FluxStandardAction<AppActionType.UPDATE_LOCALE_REJECTED> {
+  payload: {
+    error: Error;
+  };
+}
+
+export type AppAction =
+  | AppActionUpdatePage
+  | AppActionUpdateMenuState
+  | AppActionUpdateLocaleRequested
+  | AppActionUpdateLocaleResolved
+  | AppActionUpdateLocaleRejected;
 
 // Actions
 export const navigate = (page: string): AppActionUpdatePage => {
@@ -37,5 +69,26 @@ export const updateMenuState = (opened: boolean): AppActionUpdateMenuState => ({
   type: AppActionType.UPDATE_MENU_STATE,
   payload: {
     opened,
+  },
+});
+
+export const requestLocaleUpdate = (locale: Locale): AppActionUpdateLocaleRequested => ({
+  type: AppActionType.UPDATE_LOCALE_REQUESTED,
+  payload: {
+    locale,
+  },
+});
+
+export const resolveLocaleUpdate = (localeData: LocaleData): AppActionUpdateLocaleResolved => ({
+  type: AppActionType.UPDATE_LOCALE_RESOLVED,
+  payload: {
+    localeData,
+  },
+});
+
+export const rejectLocaleUpdate = (error: Error): AppActionUpdateLocaleRejected => ({
+  type: AppActionType.UPDATE_LOCALE_REJECTED,
+  payload: {
+    error,
   },
 });
