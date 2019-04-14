ec9757fd841c5e27339ee8e60c34111e803d9c5c
Giorgio Garasto <giorgio@garasto.it>
2019-04-14 19:55:44 +0200

    feat: Tweak wizard steps structure & add new step

--- src/renderer/locales/model.ts
+++ src/renderer/locales/model.ts
@@ -7,41 +7,47 @@ export interface LocaleData {
     placeholder: string;
   };
   wizard: {
-    page0: {
-      hi: string;
-      welcome: string;
-    };
-    page1: {
-      title: string;
-      disclaimer: {
-        introduction: {
-          title: string;
-          text: string;
+    steps: [
+      {
+        hi: string;
+        welcome: string;
+      },
+      {
+        title: string;
+        disclaimer: {
+          introduction: {
+            title: string;
+            text: string;
+          };
+          software: {
+            title: string;
+            text: string;
+          };
+          assets: {
+            title: string;
+            text: string;
+          };
+          agreement: {
+            title: string;
+            text: string;
+          };
         };
-        software: {
-          title: string;
-          text: string;
-        };
-        assets: {
-          title: string;
-          text: string;
-        };
-        agreement: {
-          title: string;
-          text: string;
-        };
-      };
-    };
-    page2: {
-      title: string;
-      text: [string, string, string, string];
-    };
-    page3: {
-      title: string;
-      text: [string, string];
-      idPlaceholder: string;
-      secretPlaceholder: string;
-    };
+      },
+      {
+        title: string;
+        text: string;
+      },
+      {
+        title: string;
+        text: [string, string, string, string];
+      },
+      {
+        title: string;
+        text: [string, string];
+        idPlaceholder: string;
+        secretPlaceholder: string;
+      },
+    ];
     actions: {
       authorize: string;
       next: string;
