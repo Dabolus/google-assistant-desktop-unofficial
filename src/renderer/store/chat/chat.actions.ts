44ac02d45fec48db4c9b5344227ea602a6cd49d4
Giorgio Garasto <giorgio@garasto.it>
2019-04-19 07:49:30 +0200

    feat: Add send message actions

--- src/renderer/store/chat/chat.actions.ts
+++ src/renderer/store/chat/chat.actions.ts
@@ -3,6 +3,10 @@ import { FluxStandardAction } from '../store.model';
 // Action types
 export enum ChatActionType {
   UPDATE_INPUT = 'UPDATE_INPUT',
+
+  SEND_MESSAGE_REQUESTED = 'SEND_MESSAGE_REQUESTED',
+  SEND_MESSAGE_RESOLVED = 'SEND_MESSAGE_RESOLVED',
+  SEND_MESSAGE_REJECTED = 'SEND_MESSAGE_REJECTED',
 }
 
 // Action interfaces
@@ -13,12 +17,51 @@ export interface ChatActionUpdateInput extends
   };
 }
 
-export type ChatAction = ChatActionUpdateInput;
+export interface ChatActionSendMessageRequested extends
+  FluxStandardAction<ChatActionType.SEND_MESSAGE_REQUESTED> {
+  payload: {
+    text: string;
+  };
+}
+
+export interface ChatActionSendMessageResolved extends
+  FluxStandardAction<ChatActionType.SEND_MESSAGE_RESOLVED> {}
+
+export interface ChatActionSendMessageRejected extends
+  FluxStandardAction<ChatActionType.SEND_MESSAGE_REJECTED> {
+  payload: {
+    error: Error;
+  };
+}
+
+export type ChatAction =
+  | ChatActionUpdateInput
+  | ChatActionSendMessageRequested
+  | ChatActionSendMessageResolved
+  | ChatActionSendMessageRejected;
 
 // Actions
 export const updateInput = (text: string): ChatActionUpdateInput => ({
   type: ChatActionType.UPDATE_INPUT,
   payload: {
     text,
+  },
+});
+
+export const requestMessageSend = (text: string): ChatActionSendMessageRequested => ({
+  type: ChatActionType.SEND_MESSAGE_REQUESTED,
+  payload: {
+    text,
+  },
+});
+
+export const resolveMessageSend = (): ChatActionSendMessageResolved => ({
+  type: ChatActionType.SEND_MESSAGE_RESOLVED,
+});
+
+export const rejectMessageSend = (error: Error): ChatActionSendMessageRejected => ({
+  type: ChatActionType.SEND_MESSAGE_REJECTED,
+  payload: {
+    error,
   },
 });
