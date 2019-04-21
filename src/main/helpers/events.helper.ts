f8f1d32647e207737d792fa17a3666be00351298
Giorgio Garasto <giorgio@garasto.it>
2019-04-21 09:39:53 +0200

    feat: Really basic Assistant conversation handling

--- src/main/helpers/events.helper.ts
+++ src/main/helpers/events.helper.ts
@@ -1,19 +1,40 @@
 import { Auth, AuthService } from '@services/auth.service';
 import { BrowserWindow, BrowserWindowConstructorOptions, Event, ipcMain } from 'electron';
+import { Assistant, TextConversation } from 'nodejs-assistant';
 import { container } from './di.helper';
 
 export class BrowserWindowWithEvents extends BrowserWindow {
   private _authService: Auth = container.get(AuthService);
+  private _assistant: Assistant;
+  private _textConversation: TextConversation;
 
   constructor(options?: BrowserWindowConstructorOptions) {
     super(options);
 
-    ipcMain.on('auth.requestAuthentication', async (_: Event, {
-      clientId = '',
-      clientSecret = '',
-    }: any) => {
-      const oauth2Client = await this._authService.getAuthenticatedClient(clientId, clientSecret);
-      this.webContents.send('auth.resolveAuthentication', oauth2Client.credentials);
-    });
+    ipcMain
+      .on('auth.requestAuthentication', async (_: Event, {
+        clientId,
+        clientSecret,
+      }: any) => {
+        try {
+          const credentials = await this._authService.getCredentials(clientId, clientSecret);
+          this._assistant = new Assistant(credentials);
+          this._textConversation = this._assistant.startTextConversation();
+          this._textConversation
+            .on('message', (text) => this.webContents.send('chat.receiveMessage', text));
+          this.webContents.send('auth.resolveAuthentication');
+        } catch (e) {
+          this.webContents.send('auth.rejectAuthentication', e);
+        }
+      })
+      .on('chat.requestSendMessage', (_: Event, text: string) => {
+        if (this._textConversation) {
+          if (this._textConversation.send(text)) {
+            this.webContents.send('chat.resolveSendMessage');
+          } else {
+            this.webContents.send('chat.rejectSendMessage', new Error('Unable to send message'));
+          }
+        }
+      });
   }
 }
