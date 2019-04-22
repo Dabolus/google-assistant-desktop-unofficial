ead27a07da6d2f4211a53c531f5fdd0a07d5c21e
Giorgio Garasto <giorgio@garasto.it>
2019-04-22 18:24:54 +0200

    refactor: Make it possible to have simple (stateless) conversations

--- src/main/helpers/events.helper.ts
+++ src/main/helpers/events.helper.ts
@@ -1,12 +1,11 @@
 import { Auth, AuthService } from '@services/auth.service';
 import { BrowserWindow, BrowserWindowConstructorOptions, Event, ipcMain } from 'electron';
-import { Assistant, TextConversation } from 'nodejs-assistant';
+import { Assistant } from 'nodejs-assistant';
 import { container } from './di.helper';
 
 export class BrowserWindowWithEvents extends BrowserWindow {
   private _authService: Auth = container.get(AuthService);
   private _assistant: Assistant;
-  private _textConversation: TextConversation;
 
   constructor(options?: BrowserWindowConstructorOptions) {
     super(options);
@@ -19,22 +18,21 @@ export class BrowserWindowWithEvents extends BrowserWindow {
         try {
           const credentials = await this._authService.getCredentials(clientId, clientSecret);
           this._assistant = new Assistant(credentials);
-          this._textConversation = this._assistant.startTextConversation();
-          this._textConversation
-            .on('message', (text) => this.webContents.send('chat.receiveMessage', text));
           this.webContents.send('auth.resolveAuthentication');
         } catch (e) {
           this.webContents.send('auth.rejectAuthentication', e);
         }
       })
-      .on('chat.requestSendMessage', (_: Event, text: string) => {
-        if (this._textConversation) {
-          if (this._textConversation.send(text)) {
-            this.webContents.send('chat.resolveSendMessage');
-          } else {
-            this.webContents.send('chat.rejectSendMessage', new Error('Unable to send message'));
+      .on('chat.requestSendMessage', async (_: Event, text: string) => {
+        if (this._assistant) {
+          this.webContents.send('chat.resolveSendMessage');
+          const { text: response } = await this._assistant.query(text);
+          if (response) {
+            this.webContents.send('chat.receiveMessage', response);
           }
+          return;
         }
+        this.webContents.send('chat.rejectSendMessage', new Error('Unable to send message'));
       });
   }
 }
