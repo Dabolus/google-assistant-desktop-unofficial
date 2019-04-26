059e988e13685218aa3abbae48d3a7b02e9423ee
Giorgio Garasto <giorgio@garasto.it>
2019-04-26 15:19:03 +0200

    feat: Actually use settings component

--- src/renderer/components/shell/shell.template.ts
+++ src/renderer/components/shell/shell.template.ts
@@ -3,14 +3,19 @@ import { Shell } from './shell.component';
 
 import '@components/bottom-bar/bottom-bar.component';
 import '@components/chat/chat.component';
+import '@components/settings/settings.component';
 import '@components/top-bar/top-bar.component';
 import '@components/wizard/wizard.component';
 
 export default function template(this: Shell) {
   return html`
     <gad-top-bar role="heading"></gad-top-bar>
-    <gad-chat></gad-chat>
-    <gad-bottom-bar></gad-bottom-bar>
+    ${this._page === 'settings' ? html`
+      <gad-settings></gad-settings>
+    ` : html`
+      <gad-chat></gad-chat>
+      <gad-bottom-bar></gad-bottom-bar>
+    `}
     <gad-wizard ?shown="${this._page === 'wizard'}"></gad-wizard>
   `;
 }
