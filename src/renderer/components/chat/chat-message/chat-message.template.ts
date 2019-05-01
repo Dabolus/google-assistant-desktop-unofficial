252c62ce3d32e5674a1767c5fb9f596608cbbbe2
Giorgio Garasto <giorgio@garasto.it>
2019-05-01 18:08:01 +0200

    feat: Add profile picture over each chat message

--- src/renderer/components/chat/chat-message/chat-message.template.ts
+++ src/renderer/components/chat/chat-message/chat-message.template.ts
@@ -1,8 +1,14 @@
+import { MessageType } from '@store/chat/chat.model';
 import { html } from 'lit-element';
 import { ChatMessage } from './chat-message.component';
 
 export default function template(this: ChatMessage) {
   return html`
-    <slot></slot>
+    <img
+      class="pic"
+      src="${this.type === MessageType.IN ? 'assets/google-assistant-logo.svg' : '' /* TODO: get user image */}">
+    <div class="bubble">
+      <slot></slot>
+    </div>
   `;
 }
