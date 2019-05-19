692e25c2e2771810ac93da20cde285460aad4554
Giorgio Garasto <giorgio@garasto.it>
2019-05-19 21:10:38 +0200

    feat: Add sendAudio method to chat service

--- src/renderer/services/chat.service.ts
+++ src/renderer/services/chat.service.ts
@@ -4,6 +4,7 @@ import { AssistantQueryOptions } from 'nodejs-assistant';
 
 export interface Chat {
   sendMessage(text: string, options?: AssistantQueryOptions): void;
+  sendAudio(audio: Buffer, options?: AssistantQueryOptions): void;
 }
 
 @injectable()
@@ -11,6 +12,13 @@ export class ChatService implements Chat {
   public sendMessage(text: string, options?: AssistantQueryOptions): void {
     ipcRenderer.send('chat.requestSendMessage', {
       text,
+      options,
+    });
+  }
+
+  public sendAudio(audio: Buffer, options?: AssistantQueryOptions): void {
+    ipcRenderer.send('chat.requestSendAudio', {
+      audio,
       options,
     });
   }
