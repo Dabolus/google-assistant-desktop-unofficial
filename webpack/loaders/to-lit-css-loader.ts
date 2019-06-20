8f08980d8fa8f2736b9891c81ef57abf9a5d5434
Giorgio Garasto <giorgio@garasto.it>
2019-06-20 22:05:27 +0200

    refactor: Turn to-lit-css-loader into a normal loader

--- webpack/loaders/to-lit-css-loader.ts
+++ webpack/loaders/to-lit-css-loader.ts
@@ -1,18 +1,10 @@
-import loaderUtils from 'loader-utils';
 import { loader } from 'webpack';
 
-export default function() {}
-
-export function pitch(this: loader.LoaderContext, remainingRequest: string) {
+export default function(this: loader.LoaderContext, source: string) {
   this.cacheable && this.cacheable();
 
   return `
     const {css} = require('lit-element');
-    const content = require(${loaderUtils.stringifyRequest(
-      this,
-      `!!${remainingRequest}`,
-    )});
-    const str = typeof content === 'string' ? content : content.toString();
-    module.exports = css([str]);
+    module.exports = css\`${source}\`;
   `;
 }
