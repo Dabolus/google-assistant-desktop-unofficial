48657e7ed2654c6af1af084c5c3c436bd3bb4b8f
Giorgio Garasto <giorgio@garasto.it>
2019-08-05 07:45:11 +0200

    feat: Add all future locales to Locale enum

--- src/renderer/store/app/app.model.ts
+++ src/renderer/store/app/app.model.ts
@@ -1,6 +1,18 @@
 export enum Locale {
   EN_US = 'en-US',
+  EN_AU = 'en-AU',
+  EN_CA = 'en-CA',
+  EN_GB = 'en-GB',
+  EN_IN = 'en-IN',
+  DE_DE = 'de-DE',
+  FR_CA = 'fr-CA',
+  FR_FR = 'fr-FR',
   IT_IT = 'it-IT',
+  JA_JP = 'ja-JP',
+  ES_ES = 'es-ES',
+  ES_MX = 'es-MX',
+  KO_KR = 'ko-KR',
+  PT_BR = 'pt-BR',
 }
 
 export interface AppState {
