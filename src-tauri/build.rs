11eb84fce4613e4b329410a7110a2d0ce1d4c01a
Giorgio Garasto <giorgio@garasto.me>
2022-05-18 19:56:23 +0200

    feat: setup assistant grpc module

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,33 @@
-fn main() {
-  tauri_build::build()
+use std::io::Result;
+use git2::{ErrorCode, Repository};
+
+fn main() -> Result<()> {
+  let googleapis = "proto";
+  match Repository::clone(
+      "https://github.com/googleapis/googleapis.git",
+      std::path::Path::new(googleapis),
+  ) {
+      Ok(_) => {
+          println!("[{}] cloned", googleapis);
+      }
+      Err(e) => match e.code() {
+          ErrorCode::Exists => println!("[{}] exists", googleapis),
+          _ => panic!(
+              "[{}] unexpected: {:?} {:?}",
+              googleapis,
+              e.code(),
+              e.message()
+          ),
+      },
+  }
+
+  tonic_build::configure()
+  .build_client(true)
+  .build_server(false)
+  .compile(
+      &["proto/google/assistant/embedded/v1alpha2/embedded_assistant.proto"],
+      &["proto"],
+  )?;
+  tauri_build::build();
+  Ok(())
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,25 @@
   windows_subsystem = "windows"
 )]
 
+// Include the `google` module, which is generated from Google protobuf definitions
+pub mod google {
+  pub mod api {
+    include!(concat!(env!("OUT_DIR"), "/google.api.rs"));
+  }
+
+  pub mod r#type {
+      include!(concat!(env!("OUT_DIR"), "/google.r#type.rs"));
+  }
+
+  pub mod assistant {
+      pub mod embedded {
+          pub mod v1alpha2 {
+              include!(concat!(env!("OUT_DIR"), "/google.assistant.embedded.v1alpha2.rs"));
+          }
+      }
+  }
+}
+
 fn main() {
   tauri::Builder::default()
     .run(tauri::generate_context!())
